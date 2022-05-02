079784f4f5c7a24ad8ba956759ceb771495e615e
Sascha Srpak <89406830+SaschaSrpak@users.noreply.github.com>
2022-05-02 17:21:41 +0200

    ProjektMapper (unfertig) v2

--- src/server/db/ProjektMapper.py
+++ src/server/db/ProjektMapper.py
@@ -32,7 +32,7 @@ class ProjektMapper(Mapper):
     def find_by_ID(self, ID):
         """Lesen eines Projekts aus der Datenbank mit der gegebenen ID"""
         cursor = self.cnx.cursor()
-        cursor.execute("SELECT Project_ID, Name, Client, Description, Last_modified_date from Projekt where Project_ID = %s", (ID,))
+        cursor.execute("SELECT Project_ID, Name, Client, Description, Last_modified_date from Projekt where Project_ID={}".format(ID))
         tuples = cursor.fetchall()
 
         for (Project_ID, Name, Client, Description, Last_modified_date) in tuples:
@@ -47,3 +47,25 @@ class ProjektMapper(Mapper):
         cursor.close()
 
         return projekt
+
+    def find_by_client(self, client):
+        """Lesen aller Projekte aus der Datenbank mit dem gegebenen Client"""
+        result = []
+        cursor = self.cnx.cursor()
+
+        cursor.execute("SELECT Project_ID, Name, Client, Description, Last_modified_date from Projekt where Client={}".format(client))
+        tuples = cursor.fetchall()
+
+        for (Project_ID, Name, Client, Description, Last_modified_date) in tuples:
+            projekt = Projekt()
+            projekt.set_id(Project_ID)
+            projekt.set_name(Name)
+            projekt.set_client(Client)
+            projekt.set_description(Description)
+            projekt.set_last_modified_date(Last_modified_date)
+            result.append(projekt)
+
+        self.cnx.commit()
+        cursor.close()
+
+        return result
