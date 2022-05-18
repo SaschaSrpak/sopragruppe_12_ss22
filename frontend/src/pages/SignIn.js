fca3f33c2a0ebe79af3100c4e68f6cb8b07f2b4c
Sascha Srpak <89406830+SaschaSrpak@users.noreply.github.com>
2022-05-18 23:23:07 +0200

    Initial Cleanup
    React-Logo entfernt, Material dependencies hinzugefügt

---
+++ frontend/src/pages/SignIn.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Typography, Grid, Button} from "@material-ui/core";
+
+
+
+class SignIn extends Component {
+    
+    
+    render() {
+    }
+}
+export default SignIn;
