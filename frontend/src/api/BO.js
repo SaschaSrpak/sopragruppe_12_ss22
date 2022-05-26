867b70e5356bff8ddc4139cc501a9855500f559e
KimKausler <kk162@hdm-stuttgart.de>
2022-05-26 14:42:31 +0200

    Update BO.js

--- frontend/src/api/BO.js
+++ frontend/src/api/BO.js
@@ -1,6 +1,22 @@
+/*
+Basis Klasse für alle BusinessObjects, die standardmäßig ein ID Feld hat
+*/
+
 export default class BusinessObject {
+
+    // Konstruktor
     constructor() {
         this.id = 0;
+    }
+
+    // Setzen einer ID für das jeweilige BusinessObject
+    setID(aID){
+        this.id = aID;
+    }
+
+    // Gibt die ID des BusinessObjects zurück
+    getID(){
+        return this.id;
     }
 }
 
