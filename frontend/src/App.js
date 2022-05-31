abfcf1b10a16ca6d9384fb25c8ab057c34bc1e85
Sascha Srpak <89406830+SaschaSrpak@users.noreply.github.com>
2022-05-31 14:42:01 +0200

    Header base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import { Card, Typography } from '@mui/material';
+import firebaseConfig from './firebaseconfig';
+import { onAuthStateChanged } from 'firebase/auth';
+import Header from './layout/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Card>
-          <Typography variant="h1">
-            Hello World
-          </Typography>
-        </Card>
-      </header>
-    </div>
-  );
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      appError: null,
+      authError: null,
+      loading: [],
+      newUser: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <Card>
+            <Typography variant="h1">
+              Hello World
+            </Typography>
+          </Card>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
--- frontend/src/firebaseconfig.js
+++ frontend/src/firebaseconfig.js
@@ -1,4 +1,6 @@
-const firebaseconfig = {
+import { initializeApp, getApps } from "firebase/app";
+
+const firebaseConfig = {
     apiKey: "AIzaSyALX2D5j2MI2OrVBTZ-nLHeYe9QDflT-mk",
     authDomain: "sopra-6237a.firebaseapp.com",
     projectId: "sopra-6237a",
@@ -6,6 +8,15 @@ const firebaseconfig = {
     storageBucket: "sopra-6237a.appspot.com",
     messagingSenderId: "805029827580",
     appId: "1:805029827580:web:ec2280a5d3074fcd719879"
-           };
-  
-  export default firebaseConfig;
+    };
+
+
+let firebaseApp
+if(!getApps().length){
+    firebaseApp= initializeApp(firebaseConfig);
+}
+else{
+    firebaseApp= getApps()[0];
+}
+
+export default firebaseApp;
