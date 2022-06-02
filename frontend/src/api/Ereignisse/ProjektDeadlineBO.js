6043ec11f700e77e46a62e48e89c245e5711a7f4
KimKausler <kk162@hdm-stuttgart.de>
2022-06-02 15:37:27 +0200

    Update ProjektDeadlineBO.js

--- frontend/src/api/Ereignisse/ProjektDeadlineBO.js
+++ frontend/src/api/Ereignisse/ProjektDeadlineBO.js
@@ -1,12 +1,13 @@
 import ProjektDeadlineBO from "/Ereignisse/ProjektDeadlineBO";
 /** 
  *@fileOverview 
- *@author Luca Trautmann
+ *@author Luca Trautmann, Kim Kausler
 */
 
-export default class ProjektDeadlineBO{
+export default class ProjektDeadlineBO extends ProjektDeadlineBO{
 
     constructor() {
+        super()
            }
 
     
