400173a956463a11167005b7b34a61ae13f3df59
KimKausler <kk162@hdm-stuttgart.de>
2022-06-03 23:48:44 +0200

    Update EditAktivität.js

---
+++ frontend/src/components/Dienste/EditAktivität.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Error from '../Zwischenelemente/Error'
+
+
+export class EditAktivität extends Component {
+    constructor(){
+
+    }
+
+    render(){
+
+    }
+}
