6a8a734af266d99e9b43baf4779b3f349942f914
KimKausler <kk162@hdm-stuttgart.de>
2022-06-03 23:48:36 +0200

    Update NewProjekt.js

---
+++ frontend/src/components/Dienste/NewProjekt.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Error from '../Zwischenelemente/Error'
+
+
+export class NewProjekt extends Component {
+    constructor(){
+
+    }
+
+    render(){
+
+    }
+}
