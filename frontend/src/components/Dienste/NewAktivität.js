7cca89309d10c0bceeedf95c2d1d79b808dc6721
KimKausler <kk162@hdm-stuttgart.de>
2022-06-03 23:48:33 +0200

    Update NewAktivität.js

---
+++ frontend/src/components/Dienste/NewAktivität.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Error from '../Zwischenelemente/Error'
+
+
+export class NewAktivität extends Component {
+    constructor(){
+
+    }
+
+    render(){
+
+    }
+}
