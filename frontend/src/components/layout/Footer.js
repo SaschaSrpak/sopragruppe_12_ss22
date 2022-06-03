a8654c7702678ef48a90e8a112efa0f23fbaec5f
KimKausler <kk162@hdm-stuttgart.de>
2022-06-03 10:44:40 +0200

    Update Footer.js

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+/*
+ *@fileOverview 
+ *@author 
+*/
+
+export class Footer extends Component{
+    constructor(props){
+        super(probs)
+
+
+        this.state= {
+
+        }
+    }
+
+    render() {
+        
+          const { classes, apiUser } = this.props;
+          
+          return
+      }
+    }
+
+
+Footer.propTypes={
+    classes: PropTypes.object.isRequired
+};
+export default Footer;
+       
