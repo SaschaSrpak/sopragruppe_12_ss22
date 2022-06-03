6d6e0339e75015c177399668bd0f0d93b9978d23
KimKausler <kk162@hdm-stuttgart.de>
2022-06-03 23:48:56 +0200

    Update DeleteBuchung.js

---
+++ frontend/src/components/Dienste/DeleteBuchung.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Error from '../Zwischenelemente/Error'
+
+
+export class DeleteBuchung extends Component {
+    constructor(){
+
+    }
+
+    render(){
+
+    }
+}
