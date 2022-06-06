7b6bd05200139d0a941589d7fe70651d4f7ce2b2
KimKausler <kk162@hdm-stuttgart.de>
2022-06-06 14:00:49 +0200

    Update Buchungen.js

--- frontend/src/components/pages/Buchungen.js
+++ frontend/src/components/pages/Buchungen.js
@@ -1,4 +1,29 @@
+import React,{component} from 'react';
+import Error from '../Zwischenelemente/Error';
+
 /** 
  *@fileOverview 
  *@author
 */
+
+export class Buchungen extends Component{
+    constructor(props){
+        super(probs);
+
+        this.state={
+            
+        }
+
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+
+    }
+
+}
+
+export default Buchungen;
