8da3bc475e2504866d14bef32267a5125ead9fcd
KimKausler <kk162@hdm-stuttgart.de>
2022-06-06 14:00:31 +0200

    Ergänzung author 6 overview

--- frontend/src/components/Dienste/EditProjekt.js
+++ frontend/src/components/Dienste/EditProjekt.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import Error from '../Zwischenelemente/Error'
 
+/** 
+ *@fileOverview 
+ *@author
+*/
 
 export class EditProjekt extends Component {
     constructor(props){
