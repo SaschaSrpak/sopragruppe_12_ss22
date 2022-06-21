3041e4d8f346ce1914ea25d34a8a13463bd6205a
KimKausler <kk162@hdm-stuttgart.de>
2022-06-21 20:56:37 +0200

    Update Auslese.js

--- frontend/src/components/pages/Auslese.js
+++ frontend/src/components/pages/Auslese.js
@@ -1,8 +1,17 @@
+import React, { Component } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import { Button, ButtonGroup } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PropTypes from 'prop-types';
+
 /** 
  *@fileOverview 
  *@author
 */
-import React, { Component } from "react";
 
 export class Auslese extends Component {
 
@@ -10,8 +19,136 @@ export class Auslese extends Component {
 
     render() {
         return (
-                <p> Hier wird eine Aufstellung aller Zeiten stehen</p>
-            )
+            <div>
+            <h1>Buchungen Auslesen</h1>
+            <Accordion>
+              <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header" 
+              >
+                <Grid container spacing={1} justify='flex-start' alignItems='center'>
+                  <Grid item>
+                    <Typography variant='body1' sx={{typography: 'heading'}}>
+                    </Typography>
+                </Grid>
+                <Grid item>
+                  <ButtonGroup variant='text' size='small'>
+                    <Button color='primary' >
+                      edit
+                    </Button>
+                    <Button color='secondary' >
+                      delete
+                      </Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid item xs />
+                <Grid item>
+                  <Typography>Projektarbeitszeiten</Typography>
+                </Grid>
+                </Grid>
+              </AccordionSummary>
+              <AccordionDetails>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion>
+              <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header" 
+              >
+                <Grid container spacing={1} justify='flex-start' alignItems='center'>
+                  <Grid item>
+                    <Typography variant='body1' sx={{typography: 'heading'}}>
+                    </Typography>
+                </Grid>
+                <Grid item>
+                  <ButtonGroup variant='text' size='small'>
+                    <Button color='primary' >
+                      edit
+                    </Button>
+                    <Button color='secondary' >
+                      delete
+                      </Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid item xs />
+                <Grid item>
+                  <Typography>Pausen</Typography>
+                </Grid>
+                </Grid>
+              </AccordionSummary>
+              <AccordionDetails>
+              </AccordionDetails>
+            </Accordion>
+
+
+            <Accordion>
+              <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header" 
+              >
+                <Grid container spacing={1} justify='flex-start' alignItems='center'>
+                  <Grid item>
+                    <Typography variant='body1' sx={{typography: 'heading'}}>
+                    </Typography>
+                </Grid>
+                <Grid item>
+                  <ButtonGroup variant='text' size='small'>
+                    <Button color='primary' >
+                      edit
+                    </Button>
+                    <Button color='secondary' >
+                      delete
+                      </Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid item xs />
+                <Grid item>
+                  <Typography>Kommen</Typography>
+                </Grid>
+                </Grid>
+              </AccordionSummary>
+              <AccordionDetails>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion>
+              <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header" 
+              >
+                <Grid container spacing={1} justify='flex-start' alignItems='center'>
+                  <Grid item>
+                    <Typography variant='body1' sx={{typography: 'heading'}}>
+                    </Typography>
+                </Grid>
+                <Grid item>
+                  <ButtonGroup variant='text' size='small'>
+                    <Button color='primary' >
+                      edit
+                    </Button>
+                    <Button color='secondary' >
+                      delete
+                      </Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid item xs />
+                <Grid item>
+                  <Typography>Gehen</Typography>
+                </Grid>
+                </Grid>
+              </AccordionSummary>
+              <AccordionDetails>
+              </AccordionDetails>
+            </Accordion>
+            
+        </div>
+                
+        )
     }
 }
 
