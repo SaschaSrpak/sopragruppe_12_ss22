1ff01c200818bcd7c1c04abfa27f2a9418f26a20
LucaTrautmann <lt045@hdm-stuttgart.de>
2022-06-29 15:24:33 +0200

    Homepage und about geupdated

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -11,15 +11,29 @@ export class Home extends Component {
 
 
     render() {
-        
+
         return (
-                <div >
-                <p> wir sind auf der homepage</p>
-                
-                <picture>
-                <img src="https://i.kym-cdn.com/photos/images/facebook/001/399/076/d29.jpg" />
-                </picture>
-                </div>
+            <div >
+                <h1> Software-Praktikum im SS 2022 </h1>
+                <h4> Modul: 335138 Software-Praktikum ; Prüfungsleistung: KMP <br></br>
+                    Prof. Dr. Thies und Prof. Dr. Kunz</h4><br/>
+                <h4>Einleitung </h4>
+                <p>Ziel der Lehrveranstaltung ist eine Vertiefung der in
+                    den vorangegangenen Semestern vermittelten
+                    Grundlagen zur Software-Entwicklung und zu Datenbanken. Im Zuge dessen sollen im Rahmen der
+                    Veranstaltungen einerseits weitere Techniken vermittelt werden. Andererseits sollen diese Techniken
+                    von allen Studierenden im Kontext eines Projekts angewendet werden. Da Software stets ein Ergebnis
+                    von Teams ist, sollen auch in diesen Veranstaltungen
+                    die Projektarbeiten im Gruppenkontext durchgeführt werden.</p><br/>
+                <h4>Zielsystems </h4>
+                <p>Jede Gruppe hat die Aufgabe, ein verteiltes System zur kollaborativen Zeiterfassung
+                    und Auswertung von Projektarbeit zu realisieren. <br />
+                    Das System soll die Arbeitszeiterfassung durch Projektbeteiligte sowie
+                    deren Auswertung durch Benutzer ermöglichen.<br />
+                    Die Applikation soll Personen unterstützen, ihre Arbeitsleistung zu Projekten und deren Aktivitäten zu
+                    verwalten und für Benutzer verlässlich auswertbar
+                    zu machen.</p>
+            </div>
         )
     }
 }
