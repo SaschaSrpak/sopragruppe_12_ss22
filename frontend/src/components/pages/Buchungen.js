7ebee1d93d30ba9883f3804c2d3e5d3558123757
LucaTrautmann <lt045@hdm-stuttgart.de>
2022-06-30 21:21:24 +0200

    Update Buchungen.js

--- frontend/src/components/pages/Buchungen.js
+++ frontend/src/components/pages/Buchungen.js
@@ -19,9 +19,9 @@ import { PauseBO } from '../../api';
  *@author Kim Kausler
 */
 
-export class Buchungen extends Component{
-  
-  constructor(props){
+export class Buchungen extends Component {
+
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -29,10 +29,10 @@ export class Buchungen extends Component{
       selectedActivities: null,
       activity: props.activity,
       buchung: null,
-      beginnA: null, 
+      beginnA: null,
       endeA: null,
       pause: null,
-      beginnP: null, 
+      beginnP: null,
       endeP: null,
       activitydescription: null,
       pausedescription: null
@@ -43,150 +43,173 @@ export class Buchungen extends Component{
 
 
 
-  kommenButtonClicked = event =>{
+  kommenButtonClicked = event => {
+    
     let newKommen = new KommenBO();
     newKommen.event_name = "kommen";
     newKommen.time_of_event = this.getLocalTime();
-    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result)=>{
-        console.log(result.id)
-        SystemAPI.getAPI().commitKommenTransaction(result.id , newKommen)
+    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result) => {
+      console.log(result.id)
+      SystemAPI.getAPI().commitKommenTransaction(result.id, newKommen).then ((result) => {
+        alert("Kommen wurde gebucht") 
+      })
     })
 
 
   }
 
-    gehenButtonClicked = event =>{
+  gehenButtonClicked = event => {
+    
     let newGehen = new GehenBO();
     newGehen.event_name = "gehen";
     newGehen.time_of_event = this.getLocalTime();
 
-    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result)=>{
-        console.log(result.id)
-        SystemAPI.getAPI().commitGehenTransaction(result.id , newGehen)
+    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result) => {
+      console.log(result.id)
+      SystemAPI.getAPI().commitGehenTransaction(result.id, newGehen).then ((result) => {
+        alert("Gehen wurde gebucht") 
+      })
+      
     })
-    }
-
-    bookPauseClicked = event =>{
-        SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result)=>{
-
-            SystemAPI.getAPI().commitPauseTransaction(result.id, this.state.pausedescription, this.state.beginnP, this.state.endeP)
-        })
-
-    }
-
-
-    getLocalTime = () => {
-      var tzoffset = (new Date()).getTimezoneOffset() * 60000;
-      var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -5);
-      console.log(localISOTime)
-    return localISOTime
-    }
-
-
-componentDidMount() {
-    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result)=> {
-        console.log(result.id)
-         SystemAPI.getAPI().getActivitiesForPerson(result.id).then((activities) => {
-            this.setState({
-            activities: activities
-
-        })
-
-    })})
-  }
     
+  }
+
+  bookPauseClicked = event => {
+    
+    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result) => {
+
+      SystemAPI.getAPI().commitPauseTransaction(result.id, this.state.pausedescription, this.state.beginnP, this.state.endeP).then ((result) => {
+        alert("Pause wurde gebucht") 
+      })
+    })
+
+  }
+
+
+  getLocalTime = () => {
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -5);
+    console.log(localISOTime)
+    return localISOTime
+  }
+
+
+  componentDidMount() {
+    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result) => {
+      console.log(result.id)
+      SystemAPI.getAPI().getActivitiesForPerson(result.id).then((activities) => {
+        this.setState({
+          activities: activities
+
+        })
+
+      })
+    })
+  }
+
   handleChange = event => {
     this.setState({
-        selectedActivities: event.target.value
+      selectedActivities: event.target.value
     })
   }
-  
+
   bookBuchung = () => {
 
-    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result)=>{
-        SystemAPI.getAPI().commitProjectWorktimeTransaction(result.id, this.state.selectedActivities)
+    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result) => {
+      SystemAPI.getAPI().commitProjectWorktimeTransaction(result.id, this.state.selectedActivities).then ((result) => {
+        alert("Aktivität wurde gebucht") 
+      })
     })
   }
 
 
-    bookBuchung = event => {
+  bookBuchung = event => {
     console.log(this.state.selectedActivities)
-        console.log(this.state.beginnA)
-        console.log(this.state.activitydescription)
-        console.log(this.state.endeA)
+    console.log(this.state.beginnA)
+    console.log(this.state.activitydescription)
+    console.log(this.state.endeA)
 
-      SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result)=>{
-          SystemAPI.getAPI().commitProjectWorktimeTransaction(result.id, this.state.activitydescription ,this.state.selectedActivities, this.state.beginnA, this.state.endeA )
-      })
-    }
+    SystemAPI.getAPI().getPersonByFirebaseID(this.props.user.uid).then((result) => {
+      SystemAPI.getAPI().commitProjectWorktimeTransaction(result.id, this.state.activitydescription, this.state.selectedActivities, this.state.beginnA, this.state.endeA)
+    })
+  }
 
-    render(){
-      const {activities, selectedActivities} = this.state;
-      return(
-          <div>
-                  <div style={{textAlign: "center"}}>
-                  <h1>Buchungen</h1>
-                  <Button variant="contained" onClick={() => this.kommenButtonClicked()} > Kommen </Button>
-                  <p></p>
+  render() {
+    const { activities, selectedActivities } = this.state;
+    return (
+      <div>
+        <div style={{ textAlign: "center" }}>
+          <h1>Buchungen</h1>
+          <Button variant="contained" onClick={() => this.kommenButtonClicked()} > Kommen </Button>
+          <p></p>
 
-                  <Button variant="contained" onClick={() => this.gehenButtonClicked()} > Gehen </Button>
-                  </div>
-  
-                  <Divider sx={{margin:"20px"}}/>
-              
-                  <Stack 
-                  direction={{ sm: 'row' }}
+          <Button variant="contained" onClick={() => this.gehenButtonClicked()} > Gehen </Button>
+        </div>
 
-                  >
-                    <FormControl fullWidth>
-                      <InputLabel >Aktivität</InputLabel>
-                      <Select 
-                      id="Aktivität"
-                      label="Aktivität"
-                      onChange={this.handleChange}
-                      >
-                        {activities.map((activities) => {
-                        return (
-                        <MenuItem key={activities.id} value={activities.id}>
-                            {activities.activity_name}
-                        </MenuItem>)
-                        })}
-                      </Select>
-                    
-                    <div><br></br></div>
-                    <TextField required id="outlined-required" label="Aktivitätsbeschreibung"  onChange={(event) => this.setState({activitydescription: event.target.value})} />
-                    <div><br></br></div>
-                    <TextField id="outlined-basic"  variant="outlined" type="datetime-local" label="Beginn"  onChange={(event) => this.setState({beginnA: event.target.value})} InputLabelProps={{shrink: true,}}/>
-                    <div><br></br></div>
-                    <TextField id="outlined-basic"  variant="outlined" type="datetime-local" label="Ende" onChange={(event) => this.setState({endeA: event.target.value})} InputLabelProps={{shrink: true,}}/>
-                    <div><br></br></div>
-                    <Button variant="contained" onClick={() => this.bookBuchung()}>  Aktivität Buchen </Button>
+        <Divider sx={{ margin: "20px" }} />
 
-                    </FormControl>
-                    </Stack>
-              
-                  <Divider sx={{margin:"20px"}}/>
-  
-                  <Stack 
-                  direction={{ sm: 'row' }}
-                  >
-                    <FormControl fullWidth>
-                    <h2>Pause</h2>
+        <Stack
+          direction={{ sm: 'row' }}
+        >
+          <FormControl fullWidth>
+            <h2>Pause</h2>
 
-                    <TextField required id="outlined-required" label="Required" defaultValue="Pause Beschreibung" onChange={(event) => this.setState({pausedescription: event.target.value})}/>
-                    <div><br></br></div>
-                    <TextField id="outlined-basic" label="Beginn" variant="outlined" type="datetime-local" onChange={(event) => this.setState({beginnP: event.target.value})} InputLabelProps={{shrink: true,}}/>
-                    <div><br></br></div>
-                    <TextField id="outlined-basic" label="Ende" variant="outlined" type="datetime-local" onChange={(event) => this.setState({endeP: event.target.value})} InputLabelProps={{shrink: true,}}/>
-                    <div><br></br></div>
-                    <Button variant="contained" onClick={() => this.bookPauseClicked()} >Pause Buchen </Button>
+            <TextField required id="outlined-required" label="Required" defaultValue="Pause Beschreibung" onChange={(event) => this.setState({ pausedescription: event.target.value })} />
+            <div><br></br></div>
+            <TextField id="outlined-basic" label="Beginn" variant="outlined" type="datetime-local" onChange={(event) => this.setState({ beginnP: event.target.value })} InputLabelProps={{ shrink: true, }} />
+            <div><br></br></div>
+            <TextField id="outlined-basic" label="Ende" variant="outlined" type="datetime-local" onChange={(event) => this.setState({ endeP: event.target.value })} InputLabelProps={{ shrink: true, }} />
+            <div><br></br></div>
+            <Button variant="contained" onClick={() => this.bookPauseClicked()} >Pause Buchen </Button>
 
-                    </FormControl>
-                  </Stack>
-          </div>
-      )
-    }
-    
+          </FormControl>
+        </Stack>
+        <Divider sx={{ margin: "20px" }} />
+        <h2>Aktivität</h2>
+        <Stack
+          direction={{ sm: 'row' }}
+          
+        >
+          
+          <FormControl fullWidth>
+          
+            <InputLabel >Aktivität</InputLabel>
+            <Select
+              id="Aktivität"
+              label="Aktivität"
+              onChange={this.handleChange}
+            >
+              {activities.map((activities) => {
+                return (
+                  <MenuItem key={activities.id} value={activities.id}>
+                    {activities.activity_name}
+                  </MenuItem>)
+              })}
+            </Select>
+
+            <div><br></br></div>
+
+            <TextField required id="outlined-required" label="Aktivitätsbeschreibung" onChange={(event) => this.setState({ activitydescription: event.target.value })} />
+            <div><br></br></div>
+            <TextField id="outlined-basic" variant="outlined" type="datetime-local" label="Beginn" onChange={(event) => this.setState({ beginnA: event.target.value })} InputLabelProps={{ shrink: true, }} />
+            <div><br></br></div>
+            <TextField id="outlined-basic" variant="outlined" type="datetime-local" label="Ende" onChange={(event) => this.setState({ endeA: event.target.value })} InputLabelProps={{ shrink: true, }} />
+            <div><br></br></div>
+            <Button variant="contained" onClick={() => this.bookBuchung()}>  Aktivität Buchen </Button>
+            <div><br></br></div>
+
+          </FormControl>
+        </Stack>
+
+
+
+
+        <div><br></br></div>
+        <div><br></br></div>
+      </div>
+    )
+  }
+
 }
 
 export default Buchungen;
