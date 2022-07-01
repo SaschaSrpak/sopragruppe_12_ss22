19c975a814257b560d0b1851d58bc4f86b7230b1
KimKausler <kk162@hdm-stuttgart.de>
2022-07-01 10:46:55 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -284,7 +284,7 @@ class PersonOperations(Resource):
 @timesystem.param('id', 'Die Firebase_ID des Personen Objekts')
 class PersonFirebaseOperations(Resource):
     @timesystem.marshal_with(person)
-    @secured
+   # @secured
     def get(self, id):
         """
         Gibt eine bestimmte Person aus.
